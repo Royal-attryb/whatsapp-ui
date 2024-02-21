4d96124946dfea582d408963229620fc7b78426b
Royal Garg <royalgarg36@gmail.com>
2024-02-21 17:40:17 +0530

    changed styling

--- whatsapp-mobile-ui/src/components/PreviewBody.jsx
+++ whatsapp-mobile-ui/src/components/PreviewBody.jsx
@@ -11,7 +11,7 @@ const PreviewBody = () => {
         <h1 className='event'>Valentines Day Sale</h1>
       </div>
       <div className='previewbody-content'>
-        <Main os="android"/>
+        <Main os="ios"/>
       </div>
     </div>
   )
